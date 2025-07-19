0600bd0f51a93328a407057febd41069a876d1b2
NpmPanic <pegasus.itservices.official@gmail.com>
2025-07-19 22:55:19 +0300

    nitial commit

--- src/components/EditProductsDrawer.vue
+++ src/components/EditProductsDrawer.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive, watch } from 'vue'
 import { Close } from '@element-plus/icons-vue'
+import { ref } from 'vue'
 
 const props = defineProps({
 	modelValue: Boolean,
@@ -12,16 +13,36 @@ const emit = defineEmits(['update:modelValue', 'save'])
 // Локальная копия товара
 const localProduct = reactive({
 	id: '',
-	name: '',
-	price: '',
 	count: 1,
 	count_name: '',
+	name: '',
+	second_name: '',
+	title: '',
+	soft: '',
+	nfc: '',
+	wireless_charger: '',
+	sd_card: '',
+	screen_diagonal: '',
+	display_type: '',
+	refresh_rate: '',
+	sim_type: '',
+	sim_count: '',
+	waterproof: '',
+	cpu: '',
+	battery_capacity: '',
+	equipment: '',
+	availability: '',
+	price: 0,
+	img: '',
 	warehouse: {
 		number: '',
 		place: '',
 		count: '',
 	},
 })
+
+const availabilityValues = ref(['Так', 'Ні'])
+const productAvailabilityValues = ref(['В  наявності', 'Закінчився'])
 
 // Копирование значений из props при открытии в localProduct
 watch(
@@ -30,7 +51,6 @@ watch(
 		if (val && props.product) {
 			Object.assign(localProduct, JSON.parse(JSON.stringify(props.product)))
 		}
-		console.dir(localProduct)
 	}
 )
 
@@ -68,10 +88,27 @@ const saveChanges = () => {
 				</div>
 				<div class="mb-5">
 					<div class="mb-4">
-						<span>Артикуль</span>
+						<span>Наявність товару</span>
+					</div>
+					<el-select
+						v-model="localProduct.availability"
+						placeholder="Обрати"
+						size="large"
+					>
+						<el-option
+							v-for="item in productAvailabilityValues"
+							:key="item"
+							:label="item"
+							:value="item"
+						/>
+					</el-select>
+				</div>
+				<div class="mb-5">
+					<div class="mb-4">
+						<span>Операційна система</span>
 					</div>
 					<el-input
-						v-model="localProduct.id"
+						v-model="localProduct.soft"
 						style="width: 100%"
 						size="large"
 						placeholder="Заповніть данні"
@@ -80,54 +117,91 @@ const saveChanges = () => {
 				</div>
 				<div class="mb-5">
 					<div class="mb-4">
-						<span>Назва товару</span>
+						<span>NFC модуль</span>
 					</div>
-					<el-input
-						v-model="localProduct.name"
-						style="width: 100%"
+					<el-select
+						v-model="localProduct.nfc"
+						placeholder="Обрати"
 						size="large"
-						placeholder="Заповніть данні"
-						clearable
-					/>
-				</div>
-				<div class="mb-5">
-					<div class="mb-4">
-						<span>Ціна товару</span>
-					</div>
-					<el-input
-						v-model="localProduct.price"
-						style="width: 100%"
-						size="large"
-						placeholder="Заповніть данні"
-						clearable
-					/>
+					>
+						<el-option
+							v-for="item in availabilityValues"
+							:key="item"
+							:label="item"
+							:value="item"
+						/>
+					</el-select>
 				</div>
 
-				<div class="flex mb-5 gap-5">
-					<div class="w-full">
-						<div class="mb-4">
-							<span>Кількість товару</span>
-						</div>
-						<el-input-number
-							v-model="localProduct.count"
-							:min="1"
-							:max="100"
-							size="large"
-							style="width: 100%"
-						/>
+				<div class="mb-5">
+					<div class="mb-4">
+						<span>SD карта</span>
 					</div>
-					<div class="w-full">
-						<div class="mb-4">
-							<span>Одиниці товару</span>
-						</div>
-						<el-input
-							v-model="localProduct.count_name"
-							style="width: 100%"
-							size="large"
-							placeholder="Заповніть данні"
-							clearable
+					<el-select
+						v-model="localProduct.sd_card"
+						placeholder="Обрати"
+						size="large"
+					>
+						<el-option
+							v-for="item in availabilityValues"
+							:key="item"
+							:label="item"
+							:value="item"
 						/>
+					</el-select>
+				</div>
+
+				<div class="mb-5">
+					<div class="mb-4">
+						<span>Бездротова зарядка</span>
 					</div>
+					<el-select
+						v-model="localProduct.wireless_charger"
+						placeholder="Обрати"
+						size="large"
+					>
+						<el-option
+							v-for="item in availabilityValues"
+							:key="item"
+							:label="item"
+							:value="item"
+						/>
+					</el-select>
+				</div>
+				<div class="mb-5">
+					<div class="mb-4">
+						<span>Тип SIM-карти</span>
+					</div>
+					<el-input
+						v-model="localProduct.sim_type"
+						style="width: 100%"
+						size="large"
+						placeholder="Заповніть данні"
+						clearable
+					/>
+				</div>
+				<div class="mb-5">
+					<div class="mb-4">
+						<span>Кількість SIM-карт</span>
+					</div>
+					<el-input-number
+						v-model="localProduct.sim_count"
+						:min="1"
+						:max="100"
+						style="width: 100%"
+					/>
+				</div>
+				<div class="mb-5">
+					<div class="mb-4">
+						<span>Комплектація</span>
+					</div>
+					<el-input
+						v-model="localProduct.equipment"
+						style="width: 100%"
+						size="large"
+						placeholder="Заповніть данні"
+						clearable
+					/>
 				</div>
 			</div>
 		</template>
